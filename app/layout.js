96493842c63f527824ccb5260c83a3326af06488
destebanshops <destebanshops@gmail.com>
2026-06-24 00:17:44 -0500

    chore: usar favicon SVG de 6ixstars

--- app/layout.js
+++ app/layout.js
@@ -79,13 +79,13 @@ export const metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.ico', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
       { url: '/icon-32.png', sizes: '32x32', type: 'image/png' },
       { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
     ],
     apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }],
-    shortcut: '/favicon.ico',
+    shortcut: '/favicon.svg',
   },
   openGraph: {
     siteName: '6ixstars',
