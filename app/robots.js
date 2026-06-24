aa06277af3f061b9469ecb81543b1b3ab05c3b09
destebanshops <destebanshops@gmail.com>
2026-06-24 00:19:13 -0500

    chore: deshabilitar indexación hasta lanzamiento

--- app/robots.js
+++ app/robots.js
@@ -2,20 +2,6 @@ import { SITE_URL } from '@/lib/site';
 
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: [
-          '/api/',
-          '/admin/',
-          '/checkout',
-          '/order-confirm',
-          '/wishlist',
-        ],
-      },
-    ],
-    sitemap: `${SITE_URL}/sitemap.xml`,
-    host: SITE_URL,
+    rules: [{ userAgent: '*', disallow: '/' }],
   };
 }
